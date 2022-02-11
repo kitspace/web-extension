dc31dd1b3f4ffaa687711ab35b727edb4bb28d71
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2022-02-11 12:55:22 +0000

    Adapt eslint config from kitspace-v2

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -7,15 +7,15 @@ const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React!
         </a>
