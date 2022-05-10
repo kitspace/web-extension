e5c3eba1f76580b38a6172ead37b3324e23bad68
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-10 12:27:40 +0100

    Make it work on firefox

--- src/containers/KicadPcbViewer/index.tsx
+++ src/containers/KicadPcbViewer/index.tsx
@@ -24,20 +24,30 @@ export interface KicadPcbViewerProps {
 
 export function KicadPcbViewer(props: KicadPcbViewerProps) {
   return (
-    <Suspense fallback={<div style={{ height: 500 }}>Loading...</div>}>
+    <Suspense fallback={<div>Loading...</div>}>
       <Viewer {...props} />
     </Suspense>
   )
 }
 
 function Viewer({ rawUrl }: KicadPcbViewerProps) {
-  const { data: svg } = useSWR(rawUrl, svgFetcher, { suspense: true })
+  let { data: svg, error } = useSWR(rawUrl, svgFetcher, { suspense: true })
+
+  if (svg == null) {
+    svg = <svg width={0} height={0} />
+  }
+
+  React.useEffect(() => {
+    if (error) {
+      console.error(error)
+    }
+  }, [error])
 
   const Viewer = React.useRef(null)
 
   React.useEffect(() => {
     setTimeout(() => Viewer.current.fitToViewer(), 0)
-  }, [])
+  }, [svg])
 
   return (
     <div style={{ width: '100%', height: '100vh' }}>
@@ -61,46 +71,43 @@ function Viewer({ rawUrl }: KicadPcbViewerProps) {
 }
 
 async function svgFetcher(rawUrl: string) {
-  const blob = await fetch(rawUrl).then(r => r.blob())
+  try {
+    const url = new URL(rawUrl, 'https://github.com')
+    const blob = await fetch(url.href, { mode: 'cors' }).then(r => r.blob())
 
-  const formData = new FormData()
-  formData.append('upload', blob, 'x.kicad_pcb')
+    const formData = new FormData()
+    formData.append('upload', blob, 'x.kicad_pcb')
 
-  const { id } = await fetch(PROCESSOR_DOMAIN + '/process-file', {
-    method: 'POST',
-    body: formData,
-    mode: 'cors',
-    headers: HEADERS,
-  }).then(r => r.json())
+    const { id } = await fetch(PROCESSOR_DOMAIN + '/process-file', {
+      method: 'POST',
+      body: formData,
+      mode: 'cors',
+      headers: HEADERS,
+    }).then(r => r.json())
 
-  const statusUrl = `${PROCESSOR_DOMAIN}/processed/status/${id}/images/layout.svg`
+    const statusUrl = `${PROCESSOR_DOMAIN}/processed/status/${id}/images/layout.svg`
 
-  let r = await fetch(statusUrl).then(r => r.json())
+    let r = await fetch(statusUrl).then(r => r.json())
 
-  for (let i = 0; i < MAX_ITERATION; i++) {
-    r = await fetch(statusUrl).then(r => r.json())
-    if (r.status === 'done' || r.status === 'failed') {
-      break
+    for (let i = 0; i < MAX_ITERATION; i++) {
+      r = await fetch(statusUrl).then(r => r.json())
+      if (r.status === 'done' || r.status === 'failed') {
+        break
+      }
+      await delay(10)
     }
-    await delay(10)
+
+    const svgUrl = `${PROCESSOR_DOMAIN}/processed/files/${id}/images/layout.svg`
+
+    const svg = await fetch(svgUrl).then(r => r.text())
+
+    const svgHAST = parseSVG(svg)
+
+    return toH(React.createElement, svgHAST)
+  } catch (e) {
+    // all errors in this fetcher were getting swallowed so we log them
+    // explicitly
+    console.error(e)
+    throw e
   }
-
-  const svgUrl = `${PROCESSOR_DOMAIN}/processed/files/${id}/images/layout.svg`
-
-  const svg = await fetch(svgUrl).then(r => r.text())
-
-  const svgHAST = parseSVG(svg)
-
-  return toH(React.createElement, svgHAST)
-
-  //const svgDoc = new DOMParser().parseFromString(svg, 'image/svg+xml')
-
-  //const svgWidth = parseFloat(svgDoc.documentElement.getAttribute('width'))
-  //const svgHeight = parseFloat(svgDoc.documentElement.getAttribute('height'))
-  //svgDoc.documentElement.removeAttribute('width')
-  //svgDoc.documentElement.removeAttribute('height')
-  //const viewBox = svgDoc.documentElement.getAttribute('viewBox')
-
-  //const svgXML = svgDoc.documentElement.outerHTML
-  //return { svgXML, svgWidth, svgHeight, viewBox }
 }
