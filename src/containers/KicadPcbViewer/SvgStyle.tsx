acc6a62fc55520b10f48f462fa7c261f45e510a1
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-17 20:02:48 +0100

    Adjust svg style to allow for fills in copper pours

--- src/containers/KicadPcbViewer/SvgStyle.tsx
+++ src/containers/KicadPcbViewer/SvgStyle.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import kicadTheme from './kicadThemeDefault.json'
 
 const styleString =
-  `g.kicad_svg_layer {
+  `.kicad_svg_layer {
      stroke: white;
      fill: white;
+     stroke-opacity: 0.8;
+     fill-opacity: 0.8;
+   }
+   .stroked-text {
+     fill-opacity: 0;
    }
    ` +
   Object.keys(kicadTheme.board)
     .filter(key => key !== 'copper')
     .map(
       key => `
-          g.kicad_svg_layer.${key} {
+          .kicad_svg_layer.${key} {
             stroke: ${kicadTheme.board[key]};
             fill: ${kicadTheme.board[key]};
           }
@@ -21,7 +26,7 @@ const styleString =
   Object.keys(kicadTheme.board.copper)
     .map(
       key => `
-         g.kicad_svg_layer.copper.${key} {
+         .kicad_svg_layer.copper.${key} {
            stroke: ${kicadTheme.board.copper[key]};
            fill: ${kicadTheme.board.copper[key]};
          }
