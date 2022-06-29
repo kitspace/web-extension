78730d564e9ab286b8579219232e18c7563ff356
Kaspar Emanuel <kaspar@kitspace.org>
2022-06-30 00:36:43 +0100

    Add puppeteer test running

--- src/pages/Test/index.ts
+++ src/pages/Test/index.ts
@@ -12,4 +12,21 @@ const context = require.context(
 // Require each within build
 context.keys().forEach(context)
 
-mocha.run()
+mocha
+  .run()
+  .on('test end', function (test) {
+    if ('passed' === test.state) {
+      console.log('passed!', test.title)
+    } else if (test.pending) {
+      console.log('pending!', test.title)
+    } else {
+      console.error('fail!', test.title)
+      let err = test.err
+      console.error(err)
+    }
+  })
+  .on('suite end', function (suite) {
+    if (suite.root) {
+      console.log('kitspace-web-extension-suite-end')
+    }
+  })
---
+++ test-puppeteer.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const path = require('path')
+const crypto = require('crypto')
+const assert = require('assert')
+
+const extensionPath = path.join(__dirname, 'build/manifest-v3')
+
+// this is how chromium determines the extension id of an unpacked extension
+const extensionId = crypto
+  .createHash('sha256')
+  .update(extensionPath)
+  .digest('hex')
+  .split('')
+  .slice(0, 32)
+  .map(x => String.fromCharCode(parseInt(x, 16) + 97))
+  .join('')
+
+puppeteer.use(StealthPlugin())
+
+puppeteer
+  .launch({
+    headless: false,
+    executablePath: 'chromium',
+    ignoreDefaultArgs: ['--disable-extensions'],
+    args: [`--load-extension=${extensionPath}`],
+  })
+  .then(async browser => {
+    const page = await browser.newPage()
+    page
+      .on('console', message => {
+        console.log(
+          `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`,
+        )
+        if (message.type() === 'error') {
+          assert.fail(message.text())
+        }
+        if (message.text() === 'kitspace-web-extension-suite-end') {
+          browser.close()
+        }
+      })
+      .on('pageerror', ({ message }) => console.log(message))
+      .on('response', response =>
+        console.log(`${response.status()} ${response.url()}`),
+      )
+      .on('requestfailed', request =>
+        console.log(`${request.failure().errorText} ${request.url()}`),
+      )
+    await page.goto(`chrome-extension://${extensionId}/test.html`)
+  })
