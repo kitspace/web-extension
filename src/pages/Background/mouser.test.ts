fe80e087286aa180c8194c8e73f71f805553c2c3
Kaspar Emanuel <kaspar@kitspace.org>
2022-06-30 14:50:13 +0100

    Add test for adding invalid parts to mouser cart

--- src/pages/Background/mouser.test.ts
+++ src/pages/Background/mouser.test.ts
@@ -6,7 +6,12 @@ describe('Mouser', function () {
     this.timeout(10_000)
     await Mouser.init({ country: 'UK' })
   })
-  it('adds to cart', async () => {
+  it('fails to add invalid parts', async function () {
+    const lines = [{ part: 'invalid-part', quantity: 2, reference: 'test-invalid' }]
+    const result = await Mouser.addToCart(lines)
+    assert(!result.success, "didn't fail to add invalid part")
+  })
+  it('adds to cart', async function () {
     const lines = [{ part: '595-NE555P', quantity: 2, reference: 'test' }]
     const result = await Mouser.addToCart(lines)
     assert(result.success, "didn't add to cart")
